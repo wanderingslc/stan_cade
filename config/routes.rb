1f7381b2cb718313c0a7ea6dbb9e229a6b23be21
wanderingslc <stan.martineau@gmail.com>
2020-07-17 19:43:44 -0600

    Adds about and style updates

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'main/index'
   get 'main/bunny_jump'
   get 'main/dungeon'
+  get 'main/about'
   root 'main#index', as: 'home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
